0a6cff791f03e5eb861e69fc0a70f532323e791f
athy125 <atharvajosh369@gmail.com>
2021-02-21 22:35:58 +0530

    Initial coomit

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pynetviz',
+    version='0.1',
+    description='Python Network(Graph) Visualization',
+    author='Atharva Joshi',
+    author_email='',
+
+    keywords=['Network', 'Graph', 'Visualization'],
+    url="https://github.com/koorukuroo/pynetviz",
+    packages=[
+        'pynetviz',
+    ],
+    install_requires=['networkx'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
